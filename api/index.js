5b172a2836a4e5c2b7e4cfecfec94caef9f8a47c
Martina Cesca <marticesca25@gmail.com>
2024-03-27 18:07:21 -0300

    Express y Dom

--- api/index.js
+++ api/index.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express();
+const expressApp = require("../public/express/express.js" ); // Importar la aplicación Express configurada en express.js
 
-app.get("/express", (req, res) => res.send("Express on Vercel!"));
-app.get("/cliente_servidor", (req, res) => res.send("Cliente Servidor on Vercel!"));
-app.use(express.static('public'))
+app.use(expressApp);
+
+//app.get("/cliente_servidor", (req, res) => res.send("Cliente Servidor on Vercel!"));
+
+
+app.get("/cliente_servidor", (req, res) => {
+    res.sendFile("./public/cliente_servidor/index.html", { root: '.' });
+});
+app.use(express.static('public'));
 
 
 app.listen(3001, () => console.log("Server ready on port 3001."));
